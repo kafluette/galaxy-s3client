0756164bb2325b5ffb02bb3b9d123c50a706876e
Ziv Agasi <agasizm@g.cofc.edu>
2014-07-15 16:55:31 -0400

    .

--- aws.py
+++ aws.py
@@ -5,20 +5,17 @@ from subprocess import Popen, PIPE
 
 CUR_DIR = os.path.dirname(os.path.realpath(__file__))
 
-# get command line args
 accesskey = sys.argv[1]
 secretkey = sys.argv[2]
 bucket = sys.argv[3]
 remotefile = sys.argv[4]
 localfile = sys.argv[5]
 ud = sys.argv[6]
+redirect_file = sys.argv[7]
 
-#os.environ['AWS_ACCESS_KEY_ID'] = accesskey
-#os.environ['AWS_SECRET_KEY'] = secretkey
 os.environ['EC2_ACCESS_KEY'] = accesskey
 os.environ['EC2_SECRET_KEY'] = secretkey
 
-# build up the arguments
 args = ['{}/aws.pl'.format(CUR_DIR),'--insecure-aws']
 if ud == 'u':
 	args.append('put')
@@ -26,12 +23,12 @@ else:
 	args.append('get')
 args += ['{}/{}'.format(bucket,remotefile),localfile]
 
-# execute the perl script
+output_handle = open(redirect_file,'wb')
+
 awspl = Popen(args,stdout=PIPE,stderr=PIPE)
 stdout,stderr = awspl.communicate()
 
-sys.stdout.write(stdout)
-#sys.stdout.write(stderr)
-sys.stderr.write(stderr)
+output_handle.write(stdout)
+output_handle.write(stderr)
 
 os.exit(len(stderr.strip()))
